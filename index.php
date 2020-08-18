8f01de70167190fef3ae6640f4cf38c798d1394e
Diego Malavera <diegomalavera@gmail.com>
2020-08-17 23:50:25 -0500

    Ajustes method

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<?php file_put_contents( 'data.txt', var_export( $_POST, true ) . PHP_EOL, FILE_APPEND );
+<?php
+file_put_contents( 'data.txt', $_SERVER['REQUEST_METHOD'] . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', var_export( $_REQUEST, true ) . PHP_EOL, FILE_APPEND );
