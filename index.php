94592f305887e410066f2e59c96a9a8d537dd0f6
Diego Malavera <diegomalavera@gmail.com>
2021-07-27 12:36:37 -0500

    Ajuste

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
+file_put_contents( 'data.txt', "******" . PHP_EOL, FILE_APPEND );
 file_put_contents( 'data.txt', $_SERVER['HTTP_REFERER'] . PHP_EOL, FILE_APPEND );
 file_put_contents( 'data.txt', $_SERVER['REQUEST_METHOD'] . PHP_EOL, FILE_APPEND );
-file_put_contents( 'data.txt', var_export( $_REQUEST, true ) . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', 'POST_DATA' . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', var_export( $_POST, true ) . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', 'GET_DATA' . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', var_export( $_GET, true ) . PHP_EOL, FILE_APPEND );
 file_put_contents( 'data.txt', "******" . PHP_EOL, FILE_APPEND );
