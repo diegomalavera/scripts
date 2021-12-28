d3f20f11e1dee60e9bd92bf3d5cbb6cf065a5ed0
Diego Malavera <diegomalavera@gmail.com>
2021-12-28 10:43:04 -0500

    Fix

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 file_put_contents( 'data.txt', "******" . PHP_EOL, FILE_APPEND );
-file_put_contents( 'data.txt', var_export( $_SERVER, true ) . PHP_EOL, FILE_APPEND );
-file_put_contents( 'data.txt', $_SERVER['HTTP_REFERER'] . PHP_EOL, FILE_APPEND );
 file_put_contents( 'data.txt', $_SERVER['REQUEST_METHOD'] . PHP_EOL, FILE_APPEND );
-file_put_contents( 'data.txt', 'POST_DATA' . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', "******" . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', '****** START POST_DATA' . PHP_EOL, FILE_APPEND );
 file_put_contents( 'data.txt', var_export( $_POST, true ) . PHP_EOL, FILE_APPEND );
-file_put_contents( 'data.txt', 'GET_DATA' . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', '****** END POST_DATA' . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', '****** START GET_DATA' . PHP_EOL, FILE_APPEND );
 file_put_contents( 'data.txt', var_export( $_GET, true ) . PHP_EOL, FILE_APPEND );
+file_put_contents( 'data.txt', '****** END GET_DATA' . PHP_EOL, FILE_APPEND );
 file_put_contents( 'data.txt', "******" . PHP_EOL, FILE_APPEND );
